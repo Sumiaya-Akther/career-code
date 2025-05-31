d2d0087ba726c9cf59117540032fcc5b3f702824
Sumiaya Akther <sumiayaakther15@gmail.com>
2025-05-31 18:01:57 +0600

    loading with private route

---
+++ src/provider/PrivateRoute.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { AuthContext } from './AuthProvider';
+import { Navigate, useLocation } from 'react-router';
+import Loading from '../components/Loading/Loading';
+
+
+const PrivateRoute = ({children}) => {
+
+    const {user, loading} = useContext(AuthContext)
+    //console.log(user, loading);
+    const location = useLocation();
+    //console.log(location);
+     if(loading){
+        return <Loading></Loading>
+    }
+    if(user && user?.email){
+        return children;
+    }
+    return <Navigate state={location.pathname} to="/login"></Navigate>
+};
+
+export default PrivateRoute;
